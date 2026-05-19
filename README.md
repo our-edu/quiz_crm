7f51f9aa82515d37a091c9a5c2804dec5315ed16
Ibrahim Ahmed <ibrahim.ah888@gmail.com>
2026-05-19 08:01:04 +0000

    feat: initial quiz_crm app with Quiz & Question doctypes from LMS

---
+++ README.md
@@ -0,0 +1,3 @@
+# Quiz CRM
+
+Quiz and Question management for CRM. Doctypes ported from the LMS app.
---
+++ quiz_crm/hooks.py
@@ -0,0 +1,78 @@
+app_name = "quiz_crm"
+app_title = "Quiz CRM"
+app_publisher = "OurEdu"
+app_description = "Quiz and Question management for CRM"
+app_email = "dev@ouredu.com"
+app_license = "MIT"
+
+# Apps
+# ------------------
+
+# required_apps = []
+
+# Each item in the list will be shown as an app in the apps page
+# add_to_apps_screen = [
+# 	{
+# 		"name": "quiz_crm",
+# 		"logo": "/assets/quiz_crm/images/quiz_crm.png",
+# 		"title": "Quiz CRM",
+# 		"route": "/quiz_crm",
+# 	}
+# ]
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/quiz_crm/css/quiz_crm.css"
+# app_include_js = "/assets/quiz_crm/js/quiz_crm.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/quiz_crm/css/quiz_crm.css"
+# web_include_js = "/assets/quiz_crm/js/quiz_crm.js"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# DocType Class
+# ---------------
+
+# Override standard doctype classes
+# override_doctype_class = {
+# 	"ToDo": "custom_app.overrides.CustomToDo"
+# }
+
+# Document Events
+# ---------------
+
+# Hook on document methods and events
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+# 	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"quiz_crm.tasks.all"
+# 	],
+# }
+
+# Fixtures
+# ----------
+
+# fixtures = []
